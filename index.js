01d50399908f66847ea1b8d436d5d541517b938d
tinamanga <christinamanga28@gmail.com>
2025-03-15 02:30:16 +0300

    arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+//create a variable called multiply to multiply the variables
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2; // Result: 62
+//create a variable called random to generate a random integer greater than 0
+let random = Math.floor(Math.random() * 100) + 1; // Random integer greater than 0
+// creates a variable called mod that will calculate the remainder of diving variable
+let num3 = 14;
+let num4 = 5;
+let mod = num3 % num4; // Result: 4
+// create a variable called max that finds a highest number in aset
+let max = Math.max(5, 10, 15, 20, 1); // Result: 20
